a34b2516a69825c66a9b10e30e858587306d2785
electriccheffer <electriccheff@gmail.com>
2025-04-29 02:52:13 -0500

    ADDED TRACKER BULLETS TO GAME

--- Assets/Scripts/TrackerBulletMover.cs
+++ Assets/Scripts/TrackerBulletMover.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class TrackerBulletMover : MonoBehaviour
 {
+    [SerializeField] GameObject target;  
+    [SerializeField] float velocity; 
+    
+    // initial follow time
+    [SerializeField] float upwardMovementTime; 
+  
+    // death time 
+    [SerializeField] float timeToLive; 
+    float timeAlive = 0f; 
+
+    // 
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +25,31 @@ public class TrackerBulletMover : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate(){
+
+	this.timeAlive += Time.deltaTime; 
+	if(this.timeAlive >= this.timeToLive){
+	
+		Destroy(gameObject); 
+	}	
+		
+	// calculate the velocity vector based on the current position of bullet and target.  Normalize it. 
+	if(this.target != null && this.upwardMovementTime <= this.timeAlive){
+		
+		Vector3 normalizedVelocityDirection = (target.transform.position - this.transform.position).normalized; 
+		this.transform.position += (this.velocity * normalizedVelocityDirection);
+		return; 
+	}	
+	if(this.target != null && this.upwardMovementTime >= this.timeAlive){
+	
+		this.transform.position += (this.velocity * Vector3.up); 	
+	}
+    }
+
+    public void setTarget(GameObject target){
+	this.target = target; 
+
+    }
+
 }
---
+++ Assets/Scripts/TrackerDetectionSphere.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TrackerDetectionSphere : MonoBehaviour
+{
+    [SerializeField] GameObject trackerBullet; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    void OnTriggerEnter(Collider enemy){
+
+	if(enemy.gameObject.name.StartsWith("Aircraft")){
+		//
+		Debug.Log("Collision! With TrackerDetectionSphere");
+	
+		Instantiate(trackerBullet);
+		trackerBullet.GetComponent<TrackerBulletMover>().setTarget(enemy.gameObject);	
+	}
+	
+    } 
+
+}
